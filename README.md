ee37bac2d5d9a5214e653edaf6c3b046ac29ac03
Andrzej Borucki <borucki.andrzej@gmail.com>
2017-08-15 22:53:33 +0200

    not linking with LLVM

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # llvm-toy
 Small example of own compiler using LLVM
+
+
+First run ./mk.sh to crate flex and bison files.
+
+toy.cbp - Code::Blocks project
---
+++ codegen.h
@@ -0,0 +1,45 @@
+#include <stack>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/Type.h>
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/IR/DerivedTypes.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/IR/CallingConv.h>
+#include <llvm/Bitcode/BitcodeReader.h>
+#include <llvm/IR/IRPrintingPasses.h>
+#include <llvm/Analysis/LoopPass.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/ExecutionEngine/GenericValue.h>
+#include <llvm/ExecutionEngine/ExecutionEngine.h>
+#include <llvm/ExecutionEngine/MCJIT.h>
+#include <llvm/Support/raw_ostream.h>
+
+using namespace llvm;
+
+extern LLVMContext Context;
+
+class NBlock;
+
+class CodeGenBlock {
+public:
+    BasicBlock *block;
+    std::map<std::string, Value*> locals;
+};
+
+class CodeGenContext {
+    std::stack<CodeGenBlock *> blocks;
+    Function *mainFunction;
+public:
+    Module *module;
+    CodeGenContext() { module = new Module("main", Context); }
+
+    void generateCode(NBlock& root);
+    GenericValue runCode();
+    std::map<std::string, Value*>& locals() { return blocks.top()->locals; }
+    BasicBlock *currentBlock() { return blocks.top()->block; }
+    void pushBlock(BasicBlock *block) { blocks.push(new CodeGenBlock()); blocks.top()->block = block; }
+    void popBlock() { CodeGenBlock *top = blocks.top(); blocks.pop(); delete top; }
+};
